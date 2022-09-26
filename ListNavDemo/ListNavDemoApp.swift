e8f4037474e16249e362ab211e5ffe424a4621c7
Joynal Abedin <44470728+Joynal279@users.noreply.github.com>
2022-09-27 01:04:59 +0600

    Initial Commit

---
+++ ListNavDemo/ListNavDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavDemoApp.swift
+//  ListNavDemo
+//
+//  Created by JOYNAL ABEDIN on 27/9/22.
+//
+
+import SwiftUI
+
+@main
+struct ListNavDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
